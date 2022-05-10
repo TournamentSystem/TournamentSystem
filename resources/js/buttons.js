51eaa9f6081345d1e3551216ad48cb0490b39ece
Neo Hornberger <neo.hornberger@gmx.de>
2022-05-10 21:03:02 +0200

    Added button spinners

---
+++ resources/js/buttons.js
@@ -0,0 +1,17 @@
+$(function() {
+	$('.btn[data-ts-btn-loading]').on('click', function() {
+		const TYPES = ['border', 'grow'];
+		
+		let $this = $(this);
+		let type = $this.data('ts-btn-loading');
+		
+		if(!TYPES.includes(type)) {
+			type = TYPES[0];
+		}
+		
+		let $spinner = $(`<span class="spinner-${type} spinner-${type}-sm me-1" role="status" aria-hidden="true"></span>`);
+		
+		$this.prop('disabled', true);
+		$this.prepend($spinner);
+	});
+});
